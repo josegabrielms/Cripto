c1bfa0ec51d8e52d03d02ac01c6767c674a38d03
sandraosr <sandraosr@gmail.com>
2023-06-14 15:33:53 -0500

    first commit

---
+++ src/componentes/Formulario.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from '@emotion/styled';
+
+const InputSubmit= styled.input`
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+`;
+
+const Formulario = () => {
+  return (
+    <form>
+        <InputSubmit type='submit' value='Cotizar' />
+    </form>
+  )
+}
+
+export default Formulario
